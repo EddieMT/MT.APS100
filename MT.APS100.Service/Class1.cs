426fb3d37c3f1b1ff2b14b5e7aff390d137001b8
Eddie Shen <eddie.shen@merlintest.com.cn>
2017-11-07 14:19:33 +0800

    Initial solution

---
+++ MT.APS100.Service/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT.APS100.Service
+{
+    public class Class1
+    {
+    }
+}
