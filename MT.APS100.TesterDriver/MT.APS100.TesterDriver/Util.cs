3f1a0e0f26bd2fc82b94878170bc851335ad247c
Merlin <Merlin@MERLIN-PC>
2017-11-24 14:08:25 +0800

    Tester Driver

---
+++ MT.APS100.TesterDriver/MT.APS100.TesterDriver/Util.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MT.APS100.TesterDriver
+{
+    public static class Util
+    {
+        public static void WaitTime(double settlingTime)
+        {
+            Stopwatch timer = new Stopwatch();
+            long neededTicks = (long)Math.Floor(settlingTime * Stopwatch.Frequency);
+
+            timer.Restart();
+
+            do
+            {
+            } while (timer.ElapsedTicks < neededTicks);
+
+            timer.Stop();
+        }
+
+        public const string USERCAL_DIR = @"C:\MerlinTest\UserCal\";
+    }
+}
